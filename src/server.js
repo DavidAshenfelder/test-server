90ca0fdbca1a219fe00a95de1eb503134cddae1b
David Ashenfelder <dashenfelder@boomtownroi.com>
2016-12-13 14:35:11 -0500

    adding bable and changing routes to be more dynamic

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
+import parseObj from './Helpers/parser.js';
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const router = express.Router();
 const logger = require('morgan');
 const path = require('path');
 const mongoose = require('mongoose');
-const config = require('./config');
-// const config = require('./defaultConfig');
+var ObjectId = require('mongodb').ObjectID;
+const config = require('../config');
+// const config = require('../defaultConfig');
 const request = require('request');
 const moment = require('moment');
 
-const Transaction = require('./models/transaction');
+const Transaction = require('../models/transaction');
 
 const app = express();
 mongoose.connect(config.database);
@@ -30,21 +33,22 @@ const allowCrossDomain = function(req, res, next) {
 
 // Configuration
 app.use(allowCrossDomain);
-app.use(router);
 app.set('port', config.port);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);
 // app.use(express.static(path.join(__dirname, 'public')));
 
 router.param('collectionName', function(req, res, next, collectionName) {
-  console.log('DB', db.collection );
   req.collection = db.collection(collectionName);
+  console.log('req.body blah', req.body, req.query, req.params);
   next();
 });
 
+router.route('/collections')
 //get all collections
-app.get('/collections', (req, res, next) => {
+.get((req, res, next) => {
   mongoose.connection.db.listCollections().toArray(function(err, names) {
       if(err) return next(err);
       res.send(names);
@@ -56,11 +60,16 @@ router.route('/:collectionName')
 // GET /collections/:collectionName
 .get(function(req, res, next) {
   const hasQuery = !!Object.keys(req.query).length;
-  const query = req.query || {};
+  let query = req.query || {};
   if (hasQuery) {
-    console.log(req.collection);
-    req.collection.findOne({_id: query.id}, {}, (err, results) => {
-      console.log('documents', query, err);
+    // Query is passed as a string so we need to transform this if need be to
+    // the correct format (ie Number, boolean, etc), and that is what this parseObj
+    //function does.
+    if (parseObj[req.params.collectionName]) {
+      console.log('parseObj', parseObj);
+      query = parseObj[req.params.collectionName].parser(query);
+    }
+    req.collection.find(query).toArray( (err, results) => {
       if(err) return next(err);
       console.log(results);
       res.send(results);
@@ -72,76 +81,55 @@ router.route('/:collectionName')
     });
   }
 })
+
+// Add transaction
+.post((req, res, next) => {
+  const item = req.body;
+  if (item) {
+    req.collection.insert(item, function (err) {
+      if(err) return next(err);
+      res.send({ status: 200, response: { transaction, text: `Successfully saved` } });
+    });
+  }
+});
+
+
+router.route('/:collectionName/:id')
+// Get all transactions by contactId
+.get((req, res, next) => {
+  req.collection.findOne({'_id': new ObjectId(req.params.id)}, (err, result) => {
+    console.log('err', err);
+    console.log('result', result);
+    if(err) return next(err);
+    res.send(result);
+  });
+})
+
 // Delete transaction
 .delete((req, res, next) => {
-
-  const body = bodyParser(req.body);
-  console.log('body', body);
-
-  req.collection.remove({ _id: req.body.id }, (err, result) => {
+  console.log(req.body);
+  req.collection.remove({ _id: ObjectId(req.params.id) }, (err, result) => {
     if(err) return next(err);
     console.log(result);
     res.send({ status: 200, response: `Successfully deleted transaction` });
   });
-});
-
-router.route('/:collectionName/:attrKey/:keyValue')
-// Get all transactions by contactId
-.get((req, res, next) => {
-  const attrKey = req.params.attrKey;
-  const value = req.params.keyValue;
-  console.log(value);
-  const query = {
-    [attrKey]: value
-  }
-  console.log('query', query);
-  req.collection.find(query).toArray((err, documents) => {
-    console.log('err', err);
-    console.log('documents', documents);
-    if(err) return next(err);
-    res.send(documents);
-  });
-});
-
-// Get transaction by id
-app.get('/:collectionName/:id', (req, res, next) => {
-  Transaction.findOne({_id: req.params.id}, (err, transaction) => {
-    if(err) return next(err);
-    res.send(transaction);
-  });
-});
-
-// Add transaction
-app.post('/transactions', (req, res, next) => {
-    var body = req.body;
-    console.log('body', body);
-    const transaction = new Transaction({
-      name: body.name,
-      dateAdded: moment(),
-    });
-    transaction.save(function (err) {
-      if(err) return next(err);
-      res.send({ status: 200, response: { transaction, text: `Successfully saved` } });
-    });
-});
+})
 
 // update transaction
-app.put('/transaction/:id', (req, res, next) => {
-  var transaction = req.body;
-  Transaction.findOneAndUpdate({_id: req.params.id}, transaction, function(err) {
-    console.log('error');
+.put((req, res, next) => {
+  delete req.body._id;
+  var item = req.body;
+  console.log('item', item);
+  req.collection.findOneAndUpdate({_id: ObjectId(req.params.id)}, {$set: item}, function(err) {
     if(err) return next(err);
-    console.log('no error');
-    res.send({ status: 200, response: { transaction, text: `Successfully saved` } });
+    res.send({ status: 200, response: { item, text: `Successfully saved` } });
   });
 });
-
-
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-// app.use('*', function(req, res, next) {
-//   res.sendFile(__dirname + '/index.html');
-// });
+app.use('*', function(req, res, next) {
+  res.sendFile(__dirname + '/index.html');
+});
